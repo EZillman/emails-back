e140a9fea99a30dbc636d45d8699a2d89bc2e2e3
EZillman <evelina.zillman@medieinstitutet.se>
2023-03-09 23:29:18 +0100

    something

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
+const fs = require('fs');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  fs.readFile('users.txt', function(err, data) {
+    if (err) {
+      console.log('Something went wrong');
+    }
+
+    const users = JSON.parse(data)
+
+    res.send(users)
+    return;
+  })
+});
+
+router.get('/add', function(req, res, next) {
+  fs.readFile('users.txt', function(err, data) {
+    if (err) {
+      console.log('Something went wrong');
+
+      res.send('404 - Something went wrong!')
+    }
+
+    const users = JSON.parse(data)
+
+    let newUser = req.body;
+
+    users.push(newUser);
+
+    fs.writeFile('users.txt', JSON.stringify(users, null, 4), function(err) {
+      if (err) {
+        console.log('Something went wrong');
+      }
+    });
+
+    res.send(users)
+    return;
+    
+  });
 });
 
 module.exports = router;
